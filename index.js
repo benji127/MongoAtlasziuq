71cc58a9c0fa8b6ab6dc8c20974647767ec7639f
Paja <pajae@student.douglascollege.ca>
2024-03-26 17:00:27 -0700

    Added Mongo

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 const mongoose = require('mongoose');
-
-const express = require('express')
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// require('dotenv').config();
-
 const port = process.env.PORT || 3000;
 
-// Create a Schema object
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://eneapaja27:<password>@benji127test.n7ytzwh.mongodb.net/?retryWrites=true&w=majority&appName=benji127Test/', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
-// This Activitry creates the collection called activitimodels
+// Define schema
+const studentSchema = new mongoose.Schema({
+  name: String,
+  studentID: String
+});
+
+// Create model
+const Student = mongoose.model('Student', studentSchema, 'w24students');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/form.html")
+  res.sendFile(__dirname + "/form.html");
 });
 
 app.post('/', async (req, res) => {
-  // get the data from the form
+  // Get data from the form
+  const { name, studentID } = req.body;
 
-  // connect to the database and log the connection
+  // Create a new student document
+  const student = new Student({
+    name,
+    studentID
+  });
 
-  // add the data to the database
+  // Save the document to the database
+  await student.save();
 
-  // send a response to the user
-  res.send(`<h1>Document  Added</h1>`);
+  // Send a response to the user
+  res.send(`<h1>Document Added</h1>`);
 });
 
 app.listen(port, () => {
